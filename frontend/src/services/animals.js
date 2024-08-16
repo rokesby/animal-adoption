2bcb8b49f12b46032f599f5060a4abdcc306eb33
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-08-16 11:17:26 +0100

    bugfix animal.js: removed duplicated lines

--- frontend/src/services/animals.js
+++ frontend/src/services/animals.js
@@ -18,25 +18,7 @@ export const getAnimals = async () => {
       return [];
     }
 };
-// export const getsingleAnimal = async () => {
-//   const requestOptions = {
-//     method: "GET",
-//     headers: {
-//       // Authorization: `Bearer ${token}`,
-//     },
-//   };
-//   try {
-//     const response = await fetch(`${BACKEND_URL}/listings/<int:id>`, requestOptions);
-//     if (response.status !== 200) {
-//       throw new Error("Unable to fetch animals");
-//     }
-//     const data = await response.json();
-//     return data || [];
-//   } catch (error) {
-//     console.error('Error:', error);
-//     return [];
-//   }
-// };
+
 // The animal object is what is created on CreateAdvertPage
 // We pass it in through the services function here
 export const createAnimal = async (animal) => {
@@ -61,83 +43,6 @@ export const createAnimal = async (animal) => {
     throw error;
   }
 };
-10:19
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
-export const getAnimals = async () => {
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        // Authorization: `Bearer ${token}`,
-      },
-    };
-  
-    try {
-      const response = await fetch(`${BACKEND_URL}/listings`, requestOptions);
-      if (response.status !== 200) {
-        throw new Error("Unable to fetch animals");
-      }
-      const data = await response.json();
-      return data || []; 
-    } catch (error) {
-      console.error('Error:', error);
-      return [];
-    }
-};
-
-// export const getsingleAnimal = async () => {
-//   const requestOptions = {
-//     method: "GET",
-//     headers: {
-//       // Authorization: `Bearer ${token}`,
-//     },
-//   };
-
-//   try {
-//     const response = await fetch(`${BACKEND_URL}/listings/<int:id>`, requestOptions);
-//     if (response.status !== 200) {
-//       throw new Error("Unable to fetch animals");
-//     }
-//     const data = await response.json();
-//     return data || []; 
-//   } catch (error) {
-//     console.error('Error:', error);
-//     return [];
-//   }
-// };
-
-// The animal object is what is created on CreateAdvertPage
-// We pass it in through the services function here
-
-export const createAnimal = async (animal) => {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(animal),
-  };
-
-  try {
-    console.log(`Making request to: ${BACKEND_URL}/listings`);
-    const response = await fetch(`${BACKEND_URL}/listings`, requestOptions);
-
-    if (!response.ok) {
-      throw new Error("Error creating post");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Fetch error:", error);
-    throw error;
-  }
-};
-
-
-
-
 
 
 
