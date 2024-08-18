34dd5b97df54efcd5b4d5ca1891dbbefe4c6c9aa
Avni <avnisualy@hotmail.co.uk>
2024-08-18 13:20:37 +0100

    added link to image to link to individual profile

--- frontend/src/components/AnimalCard/animalcard.jsx
+++ frontend/src/components/AnimalCard/animalcard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -11,11 +12,13 @@ const dummyImage = "https://via.placeholder.com/265"; // Dummy image URL
 const AnimalCard = ({ name, age, breed, location, image, button1Text, linkUrl }) => {
   return (
     <Card sx={{ maxWidth: 380 }} style={{ margin: "2em", overflow: "hidden" }}>
+      <Link to={linkUrl} style={{ textDecoration: 'none', color: 'inherit' }}>
       <CardMedia
         sx={{ height: 265, width: "100%", objectFit: "cover" }}
         image={image || dummyImage} 
+        
       />
-
+      </Link>
 <CardContent data-testid="animal-card">
         <Typography gutterBottom variant="h5" component="div">
           {name}
